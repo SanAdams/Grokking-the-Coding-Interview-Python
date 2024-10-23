9ed38f2b1a23c6ecb8131da7ad3b59bdb57970a3
SanAdams <sebastien1adam@gmail.com>
2024-10-22 18:53:30 -0700

    reversing linked list

--- Problems/Fast and Slow Pointers/Tests and Questions/reverse linked list.py
+++ Problems/Fast and Slow Pointers/Tests and Questions/reverse linked list.py
@@ -2,5 +2,22 @@
 # Not sure I've ever reversed a linked list
 # So let's do that
 
+from linked_list import LinkedList
 
+linkedlist = LinkedList()
+linkedlist.create_linked_list([1,3,2,7])
 
+def reverse_list(list: LinkedList):
+    previous_node = None
+    next_node = None  
+    current_node = list.head
+    while current_node is not None:
+        next_node = current_node.next
+        current_node.next = previous_node
+        previous_node = current_node
+        current_node = next_node
+    return previous_node
+        
+
+reversed_list = reverse_list(linkedlist)
+print(reversed_list.data, reversed_list.next.data, reversed_list.next.next.data, reversed_list.next.next.next.data) # Expected 7 2 3 1, Actual: 7 2 3 1
