280364b893bec0e0bb76803669faa26c30259ec5
Sebastien Adam <sebastien1adam@gmail.com>
2025-01-04 20:27:18 -0800

    Reverse a linked list

---
+++ Problems/Linked List Manipulation/ReverseLinkedList.py
@@ -0,0 +1,16 @@
+class LinkedListNode:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+def reverse(head):
+    prev = None
+    next = None
+    current = head
+    
+    while current:
+      next = current.next
+      current.next = prev
+      prev = current
+      current = next
+
