5bf89317cf6cc6639c37ffd749e36f64db67d623
Sebastien Adam <sebastien1adam@gmail.com>
2025-01-26 21:08:37 -0800

    Reverse a linked list with this one weird trick

--- Problems/Linked List Manipulation/ReverseLinkedList.py
+++ Problems/Linked List Manipulation/ReverseLinkedList.py
@@ -1,17 +1,30 @@
 # Class definition for Linked List Node
-# class LinkedListNode:
-#     def __init__(self, data, next=None):
-#         self.data = data
-#         self.next = next
+class LinkedListNode:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+def print_list(ll):
+    while ll:
+      print(f'{ll.data} -->', end = " ")
+      ll = ll.next
+    print('None', end = " ")
+      
+    
 
 def reverse(head):
-    prev = None
-    next = None
-    current = head
-    
-    while current:
-      next = current.next
-      current.next = prev
-      prev = current
-      current = next
+  prev = None
+  next = None
+  current = head
 
+  while current:
+    next = current.next
+    current.next = prev
+    prev = current
+    current = next
+  return prev
+
+ll = LinkedListNode(1, LinkedListNode(2, LinkedListNode(3)))
+print_list(ll)
+print()
+print_list(reverse(ll))
